caa34c7d490baa969032756c1700e52e2fd24b9f
ShahFH <109403034+ShahFH@users.noreply.github.com>
2023-05-07 21:38:42 +0430

    Navbar implimented

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
 
-export default function App() {
+import React, { useState } from 'react';
+
+const App = () => {
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
+  const handleNavToggle = () => {
+    setIsNavOpen(!isNavOpen);
+  };
+
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+    <div className="App">
+      <nav className={isNavOpen ? 'navbar open' : 'navbar'}>
+        <div className="nav-brand">C⭕Bloge</div>
+        <button className="nav-toggle" onClick={handleNavToggle}>
+          Menu
+        </button>
+        <ul className="nav-menu">
+          <li className="nav-item">Home</li>
+          <li className="nav-item">About</li>
+          <li className="nav-item">Services</li>
+          <li className="nav-item">Contact</li>
+        </ul>
+      </nav>
     </div>
   );
-}
+};
+
+export default App;
